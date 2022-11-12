44de6e45347e2c349d6122e5ca1a36c6e6957113
nomorechokedboy <haiduongit@protonmail.com>
2022-11-12 17:24:34 +0700

    feat: update vscode ext and brew bundle

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -6,11 +6,5 @@
 #snap install code;
 ln -sr ./settings.json $HOME/.config/Code/User/;
 ln -sr ./keybindings.json $HOME/.config/Code/User/;
-#	
-#EXT=vscode-ext.sh
-#echo $EXT
-#if ! ./$EXT
-#then
-#	chmod +x $EXT
-#fi
-#./$EXT
+cat vscode-ext | xargs -L 1 code --install-extension
+
