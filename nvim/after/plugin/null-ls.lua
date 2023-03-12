f9af96e1debfa6cc4c4d63543c33ab747285e868
nomorechokedboy <haiduongit@protonmail.com>
2023-03-12 15:47:11 +0700

    feat(null-ls): made goimports work for now

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -1,33 +1,32 @@
-local null_ls = require 'null-ls'
+local null_ls = require("null-ls")
 
 require("mason-null-ls").setup({
-    ensure_installed =
-    {
-        "stylua",
-        "eslint_d",
-        "prettier",
-        "jq",
-        "hadolint",
-        "goimports",
-        -- "golines",
-        "golangci-lint",
-        "staticcheck"
-    },
-    automatic_setup = true,
+	ensure_installed = {
+		"stylua",
+		"eslint_d",
+		"prettier",
+		"jq",
+		"hadolint",
+		"goimports",
+		-- "golines",
+		-- "golangci-lint",
+		"staticcheck",
+	},
+	automatic_setup = true,
 })
 
-require 'mason-null-ls'.setup_handlers {
-    function(source_name, methods)
-        -- all sources with no handler get passed here
+require("mason-null-ls").setup_handlers({
+	function(source_name, methods)
+		-- all sources with no handler get passed here
 
-        -- To keep the original functionality of `automatic_setup = true`,
-        -- please add the below.
-        require("mason-null-ls.automatic_setup")(source_name, methods)
-    end,
-    goimports = function(source_name, methods)
-        null_ls.register(null_ls.builtins.formatting.goimports)
-    end
-}
+		-- To keep the original functionality of `automatic_setup = true`,
+		-- please add the below.
+		require("mason-null-ls.automatic_setup")(source_name, methods)
+	end,
+	goimports = function(source_name, methods)
+		null_ls.register(null_ls.builtins.formatting.goimports)
+	end,
+})
 
 -- will setup any installed and configured sources above
 null_ls.setup()
