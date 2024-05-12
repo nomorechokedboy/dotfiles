1a4f8d3cbca4343382255af438b2d77a5028443e
duonglhh1 <duonglhh1@vnpay.vn>
2024-05-12 10:19:44 +0700

    feat: remove zsh.plugins and add zsh autosuggestions

--- home.nix
+++ home.nix
@@ -402,28 +402,6 @@ in
       enable = true;
       history.extended = true;
       syntaxHighlighting.enable = true;
-      plugins = [
-        {
-          # will source zsh-autosuggestions.plugin.zsh
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.4.0";
-            sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-          };
-        }
-        {
-          name = "enhancd";
-          file = "init.sh";
-          src = pkgs.fetchFromGitHub {
-            owner = "b4b4r07";
-            repo = "enhancd";
-            rev = "v2.2.1";
-            sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
-          };
-        }
-      ];
       prezto = {
         enable = true;
         editor = {
@@ -449,6 +427,9 @@ in
         plugins = [
           {
             name = "zsh-users/zsh-history-substring-search";
+          }
+          {
+            name = "zsh-users/zsh-autosuggestions";
           }
           {
             name = "zimfw/git";
