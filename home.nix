dc4cb8e3eb41b7b5afde45f81c8df95cbddfe72c
duonglhh1 <duonglhh1@vnpay.vn>
2024-05-12 10:08:07 +0700

    feat: config zsh

--- home.nix
+++ home.nix
@@ -397,5 +397,64 @@ in
     starship = {
       enable = true;
     };
+
+    zsh = {
+      enable = true;
+      history.extended = true;
+      syntaxHighlighting.enable = true;
+      plugins = [
+        {
+          # will source zsh-autosuggestions.plugin.zsh
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.4.0";
+            sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+          };
+        }
+        {
+          name = "enhancd";
+          file = "init.sh";
+          src = pkgs.fetchFromGitHub {
+            owner = "b4b4r07";
+            repo = "enhancd";
+            rev = "v2.2.1";
+            sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
+          };
+        }
+      ];
+      prezto = {
+        enable = true;
+        editor = {
+          dotExpansion = true;
+          promptContext = true;
+        };
+        pmodules = [
+          "completion"
+          "directory"
+          "docker"
+          "editor"
+          "environment"
+          "history"
+          "prompt"
+          "spectrum"
+          "terminal"
+          "utility"
+        ];
+      };
+      zplug = {
+        enable = true;
+
+        plugins = [
+          {
+            name = "zsh-users/zsh-history-substring-search";
+          }
+          {
+            name = "zimfw/git";
+          }
+        ];
+      };
+    };
   };
 }
